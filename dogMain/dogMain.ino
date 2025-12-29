0a9cefa0c6534fc99f8c0dbf031b7ee4be86f556
Tomasz Czura <tom.czura@gmail.com>
2025-12-29 11:15:16 +0100

    Reset all servos

--- dogMain/dogMain.ino
+++ dogMain/dogMain.ino
@@ -1,44 +1,81 @@
 #include <Adafruit_PWMServoDriver.h>
 
-
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVO_MIN 120  // this is the 'minimum' pulse length count (out of 4096)
-#define SERVO_MAX 600  // this is the 'maximum' pulse length count (out of 4096)
+#define SERVO_MIN 120 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVO_MAX 600 // this is the 'maximum' pulse length count (out of 4096)
 
 // Left front leg
-#define LFL_TOP 0
-#define LFL_HIP 1
-#define LFL_KNEE 2
+#define LFL_HIP 0
+#define LFL_KNEE 1
+#define LFL_ANKLE 2
 
 // Right front leg
-#define RFL_TOP 3
 #define RFL_HIP 4
 #define RFL_KNEE 5
+#define RFL_ANKLE 6
 
 // Left back leg
-#define LBL_TOP 6
-#define LBL_HIP 7
-#define LBL_KNEE 8
+#define LBL_HIP 8
+#define LBL_KNEE 9
+#define LBL_ANKLE 10
 
 // Right back leg
-#define RBL_TOP 9
-#define RBL_HIP 10
-#define RBL_KNEE 11
+#define RBL_HIP 12
+#define RBL_KNEE 13
+#define RBL_ANKLE 14
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pwm.begin();
   pwm.setPWMFreq(60);
+
+  resetAllHips();
 }
 
-void loop() {
-  pwm.setPWM(LFL_TOP, 0, angleToPulse(90));
+void loop()
+{
+  // pwm.setPWM(LFL_HIP, 0, angleHIPulse(0));
+  // delay(2000);
+  // pwm.setPWM(RFL_HIP, 0, angleHIPulse(0));
+  // delay(2000);
+  // pwm.setPWM(LBL_HIP, 0, angleHIPulse(0));
+  // delay(2000);
+  // pwm.setPWM(RBL_HIP, 0, angleHIPulse(0));
+  // delay(2000);
+
+  // pwm.setPWM(LFL_HIP, 0, angleHIPulse(90));
+  // delay(2000);
+  // pwm.setPWM(RFL_HIP, 0, angleHIPulse(90));
+  // delay(2000);
+  // pwm.setPWM(LBL_HIP, 0, angleHIPulse(90));
+  // delay(2000);
+  // pwm.setPWM(RBL_HIP, 0, angleHIPulse(90));
+
+  // delay(2000);
+
+  // pwm.setPWM(LFL_HIP, 0, angleHIPulse(180));
+  // delay(2000);
+  // pwm.setPWM(RFL_HIP, 0, angleHIPulse(180));
+  // delay(2000);
+  // pwm.setPWM(LBL_HIP, 0, angleHIPulse(180));
+  // delay(2000);
+  // pwm.setPWM(RBL_HIP, 0, angleHIPulse(180));
+  // delay(2000);
 }
 
-
-int angleToPulse(int ang) {
+int angleHIPulse(int ang)
+{
   int pulse = map(ang, 0, 180, SERVO_MIN, SERVO_MAX);
   return pulse;
+}
+
+void resetAllHips()
+{
+  // Set all Hips to 90 degrees position
+  pwm.setPWM(LFL_HIP, 0, angleHIPulse(90));
+  pwm.setPWM(RFL_HIP, 0, angleHIPulse(90));
+  pwm.setPWM(LBL_HIP, 0, angleHIPulse(90));
+  pwm.setPWM(RBL_HIP, 0, angleHIPulse(90));
 }
